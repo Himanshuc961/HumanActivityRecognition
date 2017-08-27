9e256bd9898930e4112df42a5f3e6bbef7249751
Himanshuc961 <himanshuc961@gmail.com>
2017-08-27 20:42:33 +0530

    Sensor Acquisation Added with  axis values

--- app/src/main/java/com/himanshuc961gmail/secondapp/MainActivity.java
+++ app/src/main/java/com/himanshuc961gmail/secondapp/MainActivity.java
@@ -18,14 +18,20 @@ import mr.go.sgfilter.ZeroEliminator;
 
 public class MainActivity extends Activity implements SensorEventListener{
 
-    private TextView xText, yText, zText;
+    private TextView xText, yText, zText,gxText,gyText,gzText,mxText,myText,mzText;
     private Button btnStart, btnStop ;
-    private Sensor mySensor;
-    private SensorManager SM;
+    private Sensor mySensor,mySensor2,mySensor3;
+    private SensorManager SM,SM2,SM3;
 
     private float x = 0;
     private float y = 0;
     private float z = 0;
+    private float gx = 0;
+    private float gy = 0;
+    private float gz = 0;
+    private float mx = 0;
+    private float my = 0;
+    private float mz = 0;
     private double acc = 0;
     private int count = 0;
     private int localcount = 0;
@@ -48,18 +54,26 @@ public class MainActivity extends Activity implements SensorEventListener{
 
         // Create our Sensor Manager
         SM = (SensorManager)getSystemService(SENSOR_SERVICE);
-
+        //SM2 = (SensorManager)getSystemService(SENSOR_SERVICE);
+        //SM3= (SensorManager)getSystemService(SENSOR_SERVICE);
         // Accelerometer Sensor
         mySensor = SM.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
-
+        mySensor2 = SM.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
+        mySensor3 = SM.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
         // Register sensor Listener
         SM.registerListener(this, mySensor, SensorManager.SENSOR_DELAY_NORMAL);
-
+        SM.registerListener(this, mySensor2, SensorManager.SENSOR_DELAY_NORMAL);
+        SM.registerListener(this, mySensor3, SensorManager.SENSOR_DELAY_NORMAL);
         // Assign TextView
         xText = (TextView)findViewById(R.id.xText);
         yText = (TextView)findViewById(R.id.yText);
         zText = (TextView)findViewById(R.id.zText);
-
+        gxText = (TextView)findViewById(R.id.textView6);
+        gyText = (TextView)findViewById(R.id.textView);
+        gzText = (TextView)findViewById(R.id.textView2);
+        mxText = (TextView)findViewById(R.id.textView3);
+        myText = (TextView)findViewById(R.id.textView4);
+        mzText = (TextView)findViewById(R.id.textView5);
         // Assign Button
        /* btnStart = (Button) findViewById(R.id.btnStart);
         btnStop = (Button) findViewById(R.id.btnStop);
@@ -69,23 +83,7 @@ public class MainActivity extends Activity implements SensorEventListener{
         btnStop.setEnabled(false); */
 
         // Initializing Graph
-        series = new LineGraphSeries<>();
-        series.appendData(new DataPoint(0, 0), true, 200);
-        GraphView graph = (GraphView) findViewById(R.id.graph);
-        /* series = new LineGraphSeries<>(new DataPoint[] {
-                new DataPoint(0, 1),
-                new DataPoint(1, 5),
-                new DataPoint(2, 3),
-                new DataPoint(3, 2),
-                new DataPoint(4, 6)
-        }); */
-        graph.addSeries(series);
-        graph.getViewport().setXAxisBoundsManual(true);
-        graph.getViewport().setMinX(0);
-        graph.getViewport().setMaxX(200);
 
-        sgFilter = new SGFilter(nl, nr);
-        sgFilter.appendPreprocessor(new ZeroEliminator());
     }
 
     @Override
@@ -107,47 +105,34 @@ public class MainActivity extends Activity implements SensorEventListener{
 
     @Override
     public void onSensorChanged(SensorEvent event) {
-        x = event.values[0] ;
-        y = event.values[1] ;
-        z = event.values[2] ;
-        acc = Math.sqrt(x*x+y*y+z*z);
+        if(event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+            x = event.values[0];
+            y = event.values[1];
+            z = event.values[2];
+        }
+       if( event.sensor.getType() == Sensor.TYPE_GYROSCOPE) {
+           gx = event.values[0];
+           gy = event.values[1];
+           gz = event.values[2];
+       }
+       if(event.sensor.getType() == Sensor.TYPE_MAGNETIC_FIELD) {
+            mx = event.values[0];
+            my = event.values[1];
+            mz = event.values[2];
+        }
+
         xText.setText("X: " + x);
         yText.setText("Y: " + y);
         zText.setText("Z: " + z);
-        int i=0;
+        gxText.setText("Gx: " + gx);
+        gxText.setText("Gy: " + gy);
+        gxText.setText("Gz: " + gz);
+        mxText.setText("Mx: " + mx);
+        myText.setText("My: " + my);
+        mzText.setText("Mz: " + mz);
 
-        if (state == 1){
-            count++;
-
-            if (count > 200) {
-                for (i = 0; i < 199; i++) {
-                    data[i] = data[i + 1];
-                }
-                localcount=200;
-                data[199]=acc;
-            }
-            else{
-                localcount=count%200;
-                if(count==200) localcount=200;
-                data[count-1]=acc;
-            }
-
-
-            if(count>nl+nr) {
-                double[] smooth = sgFilter.smooth(data, SGFilter.computeSGCoefficients(nl, nr, degree));
-                series.appendData(new DataPoint(count, smooth[localcount - 1 - nl]), true, 200);
-            }
-
-            //graph.addSeries(series);
-        }
-    }
-    public void startfn(View v){
-        state = 1;
 
     }
-    public void stopfn(View v){
 
-        state = 0;
-    }
 
 }
